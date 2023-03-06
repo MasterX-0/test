23d00c602d9ba7e96e2cbe7a7a8ce68fe8ee62f1
SudoT70 <125905752+SudoT70@users.noreply.github.com>
2023-03-06 21:44:59 +0530

    Update kk.py
    Co-authored-by: MasterX <bandaradu@outlook.com>

--- kk.py
+++ kk.py
@@ -11,3 +11,15 @@ elif marks > 35:
     value = "S"
 else:
     value = "F"
+
+if marks >= 0 and marks <= 100:
+    if marks > 75:
+        value = "A"
+    elif marks > 65:
+        value = "B"
+    elif marks > 55:
+        value = "C"
+    elif marks > 35:
+        value = "S"
+    else:
+        value = "F"
